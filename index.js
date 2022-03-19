1702631cbbccabe81b84b14fc35081929bdb1216
KelvinOkuroemi <76855464+KelvinOkuroemi@users.noreply.github.com>
2022-03-19 15:26:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+/*//declarations
+let countEl = document.getElementById("counter");
+ let count = 0;
+ let saveEl = document.getElementById("save-el");
+
+ // Increments the number
+ function buttonClick(){
+     count += 1;
+     countEl.textContent = count; 
+ }
+
+ // saves the value, and displays it as text
+ function save(){
+     let saver = " " + count + " - ";
+     saveEl.textContent += saver;
+     countEl.textContent = 0;
+     count = 0;
+ }*/
+
+ 
+
+ 
+ 
+
+ 
